16b6404db9d2d2f545389741b905cab52a9996bd
Jason Barrie Morley <jbmorley@mac.com>
2020-08-03 03:58:45 +0100

    Update the setup script to configure simple logging.

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ SCRIPT_PATH = os.path.realpath(__file__)
 ROOT_DIRECTORY = os.path.dirname(SCRIPT_PATH)
 AUTOSTART_DIRECTORY = os.path.expanduser("~/.config/autostart")
 
+LOGS_DIRECTORY = os.path.expanduser("~/Logs")
+LOGS_PATH = os.path.join(LOGS_DIRECTORY, "elsewhere.log")
+
 ELSEWHERE_FILENAME = "elsewhere.py"
 DESKTOP_ENTRY_FILENAME = "elsewhere.desktop"
 
@@ -18,11 +21,18 @@ DESKTOP_ENTRY_TEMPLATE = """
 
 Type=Application
 Name=Elsewhere
-Exec=%s %s
+Exec=%s %s >> %s 2>&1
 """
 
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s", datefmt='%Y-%m-%d %H:%M:%S %z')
+
+
+def makedirs(path):
+    if os.path.exists(path):
+        return
+    logging.info("Creating '%s'...")
+    os.makedirs(path)
 
 
 def main():
@@ -34,16 +44,15 @@ def main():
     subprocess.run(["sudo", "apt", "update"])
     subprocess.run(["sudo", "apt", "install", "livestreamer"])
 
-    logging.info("Creating '%s'...", AUTOSTART_DIRECTORY)
-    try:
-        os.makedirs(AUTOSTART_DIRECTORY)
-    except FileExistsError:
-        pass
+    makedirs(AUTOSTART_DIRECTORY)
+    makedirs(LOGS_DIRECTORY)
 
     desktop_entry_path = os.path.join(AUTOSTART_DIRECTORY, DESKTOP_ENTRY_FILENAME)
     logging.info("Creating '%s'...", desktop_entry_path)
     with open(desktop_entry_path, "w") as fh:
-        desktop_entry = DESKTOP_ENTRY_TEMPLATE % (os.path.join(ROOT_DIRECTORY, ELSEWHERE_FILENAME), options.streams)
+        desktop_entry = DESKTOP_ENTRY_TEMPLATE % (os.path.join(ROOT_DIRECTORY, ELSEWHERE_FILENAME),
+                                                  options.streams,
+                                                  LOGS_PATH)
         fh.write(desktop_entry)
 
 
