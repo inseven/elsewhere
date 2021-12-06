8e79f4669a09834dbfb013ea2107f2e330c7a06c
Jason Morley <hello@jbmorley.co.uk>
2021-12-06 00:23:34 +0000

    Fix the path to the splash image.

--- splash/set.sh
+++ splash/set.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-fim --quiet splash.png --execute-script "$DIRECTORY/script.txt"
+fim --quiet "$DIRECTORY/splash.png" --execute-script "$DIRECTORY/script.txt"
