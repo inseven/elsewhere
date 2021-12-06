4e83306fe1bed36c7d4308144cb650860ad77ebf
Jason Morley <hello@jbmorley.co.uk>
2021-12-06 01:14:36 +0000

    Hard-code the path to streamlink.

--- elsewhere.py
+++ elsewhere.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(process)d] [%(l
 
 
 def livestreamer(url):
-    return subprocess.Popen(["streamlink", "--player", "cvlc --fullscreen --no-video-title-show", url, "best"])
+    return subprocess.Popen(["/usr/local/bin/streamlink", "--player", "cvlc --fullscreen --no-video-title-show", url, "best"])
 
 
 app = Flask(__name__)
