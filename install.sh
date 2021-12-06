89651343a7879c513cd33fe16b844aa064bce233
Jason Morley <hello@jbmorley.co.uk>
2021-12-06 02:00:53 +0000

    Some unattended updates setup.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Install package dependencies.
 sudo apt-get update
 xargs -a packages.txt sudo apt-get install --yes
+
+# Enable unattended updates.
+echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | sudo debconf-set-selections
+sudo dpkg-reconfigure -f noninteractive unattended-upgrades
