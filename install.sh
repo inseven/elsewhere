c9ca570ea9da48e999f66a4c2d6ca082052a0ada
Jason Morley <hello@jbmorley.co.uk>
2021-12-27 01:13:54 +0000

    fix: Don't use cron for running Elsewhere

--- install.sh
+++ install.sh
@@ -15,5 +15,6 @@ sudo dpkg-reconfigure -f noninteractive unattended-upgrades
 # Initial urls.txt configuration.
 echo "ustream.tv/channel/iss-hdev-payload" > /home/pi/projects/elsewhere/urls.txt
 
-# Configure Elsewhere to run on startup.
-(crontab -l 2>/dev/null; echo "@reboot /bin/bash /home/pi/projects/elsewhere/elsewhere") | crontab -
+
+
+
