6562a7e3703bf0da4b3ed57b70e44ca5e61f3fbf
Jason Morley <hello@jbmorley.co.uk>
2021-12-27 07:10:52 +0000

    feat: Add the version number to the splash screen

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ SPLASH_BUILD_DIRECTORY=package/elsewhere/usr/share/elsewhere/splash/images
 
 echo "Generating splash screens..."
 mkdir -p "$SPLASH_BUILD_DIRECTORY"
-convert -background black -fill white -font "$font" -size 1024x768 -pointsize 64 -gravity center label:"Elsewhere" "$SPLASH_BUILD_DIRECTORY/splash.png"
+convert -background black -fill white -font "$font" -size 1024x768 -pointsize 64 -gravity center label:"Elsewhere\n1.0.0" "$SPLASH_BUILD_DIRECTORY/splash.png"
 convert -background black -fill white -font "$font" -size 1024x768 -pointsize 32 -gravity center label:"Shutting Down..." "$SPLASH_BUILD_DIRECTORY/shutting-down.png"
 
 echo "Generating local Python cache..."
