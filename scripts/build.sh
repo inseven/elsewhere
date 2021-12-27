8977151e4d4acc6ca74263ed75774e17c276b35e
Jason Morley <hello@jbmorley.co.uk>
2021-12-27 16:19:10 +0000

    refactor: Move the build script into the scripts directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,20 +5,22 @@ set -o pipefail
 set -u
 
 SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-ROOT_DIRECTORY="$SCRIPTS_DIRECTORY"
+ROOT_DIRECTORY="$SCRIPTS_DIRECTORY/.."
 SOURCE_DIRECTORY="$ROOT_DIRECTORY/package"
 
 font="fonts/Inter/static/Inter-Thin.ttf"
 
-SPLASH_BUILD_DIRECTORY=package/elsewhere/usr/share/elsewhere/splash/images
-
 echo "Generating splash screens..."
+SPLASH_BUILD_DIRECTORY="$SOURCE_DIRECTORY/elsewhere/usr/share/elsewhere/splash/images"
+if [ -d "$SPLASH_BUILD_DIRECTORY" ] ; then
+    rm -r "$SPLASH_BUILD_DIRECTORY"
+fi
 mkdir -p "$SPLASH_BUILD_DIRECTORY"
 convert -background black -fill white -font "$font" -size 1024x768 -pointsize 64 -gravity center label:"Elsewhere" -size 100x100 -pointsize 24 label:"1.0.0" -geometry +0+240 -composite "$SPLASH_BUILD_DIRECTORY/splash.png"
 convert -background black -fill white -font "$font" -size 1024x768 -pointsize 32 -gravity center label:"Shutting Down..." "$SPLASH_BUILD_DIRECTORY/shutting-down.png"
 
 echo "Generating local Python cache..."
-export PYTHONUSERBASE="$ROOT_DIRECTORY/package/elsewhere/usr/share/elsewhere/python"
+export PYTHONUSERBASE="$SOURCE_DIRECTORY/elsewhere/usr/share/elsewhere/python"
 if [ -d "$PYTHONUSERBASE" ] ; then
     rm -r "$PYTHONUSERBASE"
 fi
