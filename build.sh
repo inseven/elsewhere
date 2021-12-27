0c7d7eab3ff3ea32d936b34d26eb0a3f2d2bf810
Jason Morley <hello@jbmorley.co.uk>
2021-12-27 02:32:19 +0000

    ci: Build a dummy package

--- build.sh
+++ build.sh
@@ -6,5 +6,11 @@ set -u
 
 font="fonts/Inter/static/Inter-Thin.ttf"
 
+echo "Generating splash screens..."
 convert -background black -fill white -font "$font" -size 1024x768 -pointsize 64 -gravity center label:"Elsewhere" splash/splash.png
 convert -background black -fill white -font "$font" -size 1024x768 -pointsize 32 -gravity center label:"Shutting Down..." splash/shutting-down.png
+
+# Build the Debian package.
+echo "Building Debian package..."
+cd package
+dpkg-deb --build elsewhere
