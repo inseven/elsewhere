556e7838ea0e57c54a5f7d06f278d13bfbca185f
Jason Morley <hello@jbmorley.co.uk>
2023-01-23 18:34:17 +0000

    Don't install the GitHub CLI as part of the build

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -22,13 +22,3 @@ mkdir -p "$PYTHONUSERBASE"
 # Install the Python dependencies
 pip3 install --user pipenv
 PIPENV_PIPFILE="$CHANGES_DIRECTORY/Pipfile" pipenv install --verbose
-
-# Install the GitHub CLI
-github_cli_url="https://github.com"`curl -s -L https://github.com/cli/cli/releases/latest | grep -o -e "/.*armv6.*tar.gz"`
-if [ -d "$GITHUB_CLI_PATH" ] ; then
-    rm -r "$GITHUB_CLI_PATH"
-fi
-mkdir -p "$GITHUB_CLI_PATH"
-curl --location "$github_cli_url" --output "cli.tar.gz"
-tar --strip-components 1 -zxv -f "cli.tar.gz" -C "$GITHUB_CLI_PATH"
-unlink "cli.tar.gz"
